f0cdd02d061c47ff7cceb999e4f01c4defd57833
lucianofprocha-crypto <lucianofprocha@me.com>
2026-09-21 21:28:36 +0200

    Add files via upload

--- ramp/sw.js
+++ ramp/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ramp-check-v1';
+const CACHE = 'ramp-check-v2';
 const FILES = ['./', './index.html', './ramp_data.js', './manifest.json',
   './icon-180.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(e){
