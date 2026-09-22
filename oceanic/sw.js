53c7b862e2284fcfc58cee419b9c032c65ba769b
lucianofprocha-crypto <lucianofprocha@me.com>
2026-09-22 20:34:59 +0200

    Add files via upload

--- oceanic/sw.js
+++ oceanic/sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'oceanic-natla-v9';
+const CACHE = 'oceanic-natla-v10';
 const FILES = ['./', './index.html', './oceanic_data.js', './bulletins/checklist-2026-09-18.pdf', './bulletins/2017-004-r2.pdf', './bulletins/2019-003-r5.pdf', './bulletins/2023-001-r6.pdf', './bulletins/2024-001.pdf', './bulletins/2025-001.pdf', './bulletins/2026-001-oesb.pdf', './jspdf.umd.min.js', './jspdf.plugin.autotable.min.js', './manifest.json',
   './icon-180.png', './icon-192.png', './icon-512.png'];
 self.addEventListener('install', function(e){
